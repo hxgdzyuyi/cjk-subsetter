c5531cdb214510fb5342b163c65d94e91289c572
yangqing <hxgdzyuyi@gmail.com>
2013-10-01 15:47:45 +0800

    添加 nokogiri

--- lib/rack/subset.rb
+++ lib/rack/subset.rb
@@ -1,15 +1,35 @@
 require 'rack'
+require 'nokogiri'
+
 module Rack
   class Subset
     def initialize(app, options={})
       @app = app
     end
 
+    def is_cjk_char(str)
+      !!(str =~ /\p{Han}|\p{Katakana}|\p{Hiragana}\p{Hangul}/)
+    end
+
+    def get_cjk_set(html)
+      doc = Nokogiri::HTML(html)
+      inner_text = doc.search('.cjk').inner_text
+      dict = Hash.new
+      inner_text.each_char do |s|
+        next unless is_cjk_char(s)
+        dict[s] = 1
+      end
+      dict.map { |key, value| key }.join
+    end
+
     def call(env)
       status, headers, body = @app.call(env)
-      append_s = "test"
       new_body = ""
-      body.each { |string| new_body << " " << string }
+      append_s = ""
+      body.each do |string|
+        append_s << get_cjk_set(string)
+        new_body << " " << string
+      end
       new_body << " " << append_s
       [status, headers, [new_body]]
     end
