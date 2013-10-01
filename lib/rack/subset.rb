a1085b25eaf290a707fc858b1326b9f9b4a92c95
yangqing <hxgdzyuyi@gmail.com>
2013-10-01 18:46:09 +0800

    Add erb

--- lib/rack/subset.rb
+++ lib/rack/subset.rb
@@ -1,10 +1,15 @@
 require 'rack'
 require 'nokogiri'
+require 'erb'
 
 module Rack
   class Subset
     def initialize(app, options={})
       @app = app
+      @subset_css_prefix = options[:subset_css_prefix]
+      @symbol_font_map = options[:symbol_font_map]
+      @font_file_dir = options[:font_file_dir]
+      @template ||= ::ERB.new ::File.read ::File.expand_path("../style.erb", __FILE__)
     end
 
     def is_cjk_char(str)
@@ -22,16 +27,33 @@ module Rack
       dict.map { |key, value| key }.join
     end
 
+    def html?(headers)
+      headers['Content-Type'] =~ /html/
+    end
+
+    def create_tempfile
+      Tempfile.new(::File.basename(@path)).tap { |f| f.close }
+    end
+
     def call(env)
+      @path = env["PATH_INFO"]
+
       status, headers, body = @app.call(env)
+      return [status, headers, body] unless html? headers
+
+      response = Rack::Response.new([], status, headers)
       new_body = ""
-      append_s = ""
+      subset_string = ""
+
       body.each do |string|
-        append_s << get_cjk_set(string)
+        subset_string << get_cjk_set(string)
         new_body << " " << string
       end
-      new_body << " " << append_s
-      [status, headers, [new_body]]
+
+      new_body.gsub!(%r{</head>}, @template.result() + "</head>")
+      new_body.gsub!(%r{</body>}, subset_string + "</body>")
+      response.write new_body
+      response.finish
     end
   end
 end
